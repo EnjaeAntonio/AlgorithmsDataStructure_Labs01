c5cc84d82e8f0a190cdf0ee9142867e6844a0953
Enjae Antonio <Enjaeantonio@outlook.com>
2023-01-10 10:32:37 -0600

    using the char.IsLetter for validation

--- Program.cs
+++ Program.cs
@@ -6,12 +6,10 @@
 
 // char[] chars = String.ToCharArray
 
+using System.Xml;
+
 Console.WriteLine("Please enter how many words you would like");
-
-
-int userWordCount = Int32.Parse(Console.ReadLine()); // Console.ReadLine doesnt work alone because it is a string.
-
-
+int userWordCount = Int32.Parse(Console.ReadLine()); 
 
 while (userWordCount <= 0)
 {
@@ -25,19 +23,25 @@ string[] userWords = new string[userWordCount];
 for (int i = 0; i < wordCount; i++)
 {
     Console.Write($"Enter word {i + 1} : ");
-    string inputWords = Console.ReadLine();
+    string enteredWords = Console.ReadLine();
 
-    while(inputWords.Length < 1)
+    while(enteredWords.Length < 1)
     {
         Console.WriteLine("Sorry, you must have at least one character");
-        inputWords = Console.ReadLine();
+        enteredWords = Console.ReadLine();
     }
-    userWords[i] = inputWords;
-    Console.WriteLine(inputWords);
+    userWords[i] = enteredWords;
 }
 
 Console.WriteLine("Enter a character: ");
 char userChar = Console.ReadKey().KeyChar;
+
+while (!char.IsLetter(userChar))
+{
+    Console.WriteLine();
+    Console.WriteLine("Please enter a character");
+    userChar = Console.ReadKey().KeyChar;
+}
 
 int count = 0;
 int totalChars = 0;
@@ -53,10 +57,9 @@ foreach (string word in userWords)
         }
     }
 }
-    
 Console.WriteLine();
 Console.WriteLine($"The word {userChar} occurs {count} times");
-
+Console.WriteLine((double)totalChars);
 if (count / (double)totalChars > 0.25)
 {
     Console.WriteLine($"The character {userChar} represents more than 25% of the total number of characters.");
